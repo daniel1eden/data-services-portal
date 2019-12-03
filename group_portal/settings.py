6147a41f56e172f33318f3c6bd4793a6801df561
daniel1eden <daniel.eden@adelphigroup.com>
2019-12-03 12:15:17 +0000

    Make changes related to deployment

--- group_portal/settings.py
+++ group_portal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=h#w+*0a163q*%=nb)wmd1^wu)m+9szykdhx23$scnp^fn^uwb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['daniel1eden1.eu.pythonanywhere.com']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/daniel1eden1/data-services-portal/assets'
